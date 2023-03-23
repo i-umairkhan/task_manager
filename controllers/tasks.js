2114df2afdfde173ebb5bfff731a9f355cf4739e
umair2607 <umair.k2607@gmail.com>
2023-03-24 01:00:27 +0500

    controllers to get all/single tasks

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,13 +1,30 @@
 const Task = require("../model/task");
 
-const getAllTasks = (req, res) => {
-  res.send("all tasks");
+// controller to get all tasks
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const getTask = (req, res) => {
-  res.send("a task");
+// controller to get a specific task
+const getTask = async (req, res) => {
+  try {
+    const taskId = req.params.id;
+    const task = await Task.findOne({ _id: taskId });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id ${taskId}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
+// controller to create a task
 const createTask = async (req, res) => {
   try {
     const task = await Task.create(req.body);
