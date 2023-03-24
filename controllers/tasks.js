8fa172cd3b96eeaf5c7f6519ff9f011dc3eed85a
umair2607 <umair.k2607@gmail.com>
2023-03-25 00:05:05 +0500

    async wrappper middelware

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,28 +1,24 @@
 const Task = require("../model/task");
+const asyncWrapper = require("../middleware/async");
 
-// controller to get all tasks
-const getAllTasks = async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.status(200).json({ tasks });
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
+// async wrapper is a miidleware to remove remove try catch block
+// getAllTasks & getTask are wrapped in async wrapper others still use try catch
 
-// controller to get a specific task
-const getTask = async (req, res) => {
-  try {
-    const taskId = req.params.id;
-    const task = await Task.findOne({ _id: taskId });
-    if (!task) {
-      return res.status(404).json({ msg: `No task with id ${taskId}` });
-    }
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
+// controller to get all tasks => With async wrapper
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({});
+  res.status(200).json({ tasks });
+});
+
+// controller to get a specific task => With async wrapper
+const getTask = asyncWrapper(async (req, res) => {
+  const taskId = req.params.id;
+  const task = await Task.findOne({ _id: taskId });
+  if (!task) {
+    return res.status(404).json({ msg: `No task with id ${taskId}` });
   }
-};
+  res.status(200).json({ task });
+});
 
 // controller to create a task
 const createTask = async (req, res) => {
--- middleware/notFound.js
+++ middleware/notFound.js
@@ -1,5 +1,5 @@
 const notFound = (req, res) => {
-  res.status(404).send("Route Not Found");
+  res.status(404).send("Route Not Found -> 404");
 };
 
 module.exports = notFound;
